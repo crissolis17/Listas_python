4fe3da8c3d48edaaadb5f8198a89e0c6ac7abc90
Cristia Solis <cri.solis@duocuc.cl>
2023-05-25 20:16:27 -0400

    listas

---
+++ Listas.py
@@ -0,0 +1,12 @@
+lista=[1,2,3,4,5,6]
+listab=[11,3,5,7,13,40]
+lista.append(7)
+lista.extend(["a","b",])#agrega elemtos a la lista
+lista.insert(2,"casa")#inserta un elemento en una posicion indicada, en el primer espacio
+lista.remove(2)#elimina elemento indicado en el parentesis
+lista.pop(3)#para eliminar un elemento en la posicion indicada, sin numero dentro del parentesis elimina el ultimo elemento de la lista
+lista.reverse()#da vueta los elmetos de la lista 
+listab.sort()#sirve para ordenar los numeros de una lista de mayor de a menor
+print(listab)
+print(lista)
+
